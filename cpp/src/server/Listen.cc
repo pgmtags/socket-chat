6261061de59827755852f0fa9b469ad4bb077106
AndrijZyn <zinukandrij@gmail.com>
2024-06-19 14:01:20 +0300

    Remove ./cpp, README

--- cpp/src/server/Listen.cc
+++
@@ -1,9 +0,0 @@
-#include "./server.h"
-
-void Listen(int sockfd, int backlog) {
-  const int res = listen(sockfd, backlog);
-  if (res < 0) {
-    printError();
-    throw std::runtime_error("Failed to listen");
-  }
-}
--- cpp/src/server/logout.cc
+++
@@ -1,6 +0,0 @@
-#include "./server.h"
-
-bool logout(const Database<User> &db, const string &uname) {
-  auto user = db.get(uname).first;
-  return db.update(user, User({uname, user.get("password"), "0"}));
-}

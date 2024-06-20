22ca34fb0882587c32ccd8a60daae48f748c2067
AndrijZyn <zinukandrij@gmail.com>
2024-06-20 13:43:05 +0300

    Beautiful update | Better displaying output
    * change clear() function to system('clear')
    * remove prompt(), clear_current_line()
    * partially fixed a vulnerability that occurred when attempting to select the name and number of more than one person
    * the EXIT command has been deleted because it is no longer needed, and Ctrl+C is now used instead.

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import sys
 import select
 import socket
-from colorama import Fore, Style
+from colorama import init, Fore, Style
+from os import system
 
 """
 By Sudhanshu Vishnoi, Update by AndrijZyn
@@ -15,16 +16,22 @@ Python socket based CLI multi-client chat (client)
 
 # TODO: add the ability to change the lobby. Create func 'change_lobby'
 # TODO: fix queue in setup_chat_room()
+# TODO: fix handle_first_connection
+# TODO: make a normal output of names
 # TODO: give the ability to write emoji in the console, or make a command that will convert certain codes into emoji,
 #  the list of codes will be displayed behind the command
 # TODO: Add user login & create DB with User information(Password, user,
 #       (maybe)ID, bonusKey (If i'll add ID user to restore the "Name"))
 #       At the moment I am unable to fix one thing when trying to implement this function (
 
+#  ./virtualEnv/bin/python3.11
+
+init(autoreset=True)  # Initialize colorama
 
 # Emoji's
-problem = "\N{anger symbol}... \n"
+problem = "\N{anger symbol} "
 angry = " \N{angry face}"
+hand = "\U0001F44B"
 
 
 class ChatClient:
@@ -32,21 +39,29 @@ class ChatClient:
     separator = '::'
     group = "default"
     username = ""
-    helpMsg = ("Available commands:"
-               "\nHELP - Show this message\nCLEAR - Clear the screen\nLIST - Display all users\nEXIT - Exit the chat")
+    helpMsg = (f"{Fore.GREEN}{Style.BRIGHT}Available commands:{Style.RESET_ALL}"
+               f"\nHELP - Show this message\nCLEAR - Clear the screen\nLIST - Display all users\n")
 
     def __init__(self, host_, port_):
         self.host = host_
         self.port = port_
         self.s = None
         self.timeout = 0.1
+        """
+        timeout = 0.1 
+        
+        If the server does not respond for longer than 100 ms, 
+        the client will be disconnected
+        """
 
     def connect(self):
         try:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.s.settimeout(self.timeout)
             self.s.connect((self.host, self.port))
-            print("Connected to server!")
+            system('clear')
+            print(f"{Fore.GREEN}{Style.BRIGHT}You have been connected to the server Home, "
+                  f"{Fore.BLUE}select{Fore.GREEN}, or create a new room")
         except socket.timeout:
             print(problem + "IDK where to dock" + angry)
             sys.exit(0)
@@ -54,21 +69,14 @@ class ChatClient:
             print(problem + f"Connection error: {e}")
             sys.exit(1)
 
-    def disconnect(self):
-        print("Disconnecting from server...")
+    def disconnect(self, status):
+        if status == "cork":
+            print("Please, Wait for the other person to validate")  # Need fix queue
+        system('clear')
+        print(f"Disconnecting... {hand}")
         if self.s:
             self.s.close()
         sys.exit(0)
-
-    @staticmethod
-    def prompt():
-        sys.stdout.write(f"{Fore.GREEN}{Style.BRIGHT}> {Fore.RESET}")
-        sys.stdout.flush()
-
-    @staticmethod
-    def clear_current_line():
-        sys.stdout.write("\033[K")
-        sys.stdout.flush()
 
     def print_list(self, title, data):
         if not data:
@@ -80,7 +88,7 @@ class ChatClient:
 
     def handle_incoming_message(self, data):
         if not data:
-            self.disconnect()
+            self.disconnect("main")
         else:
             try:
                 dt = data.split(self.separator, 1)
@@ -88,11 +96,10 @@ class ChatClient:
                     print(f"Malformed message: {data}")
                     return
                 name, msg = dt
-                self.clear_current_line()
 
                 if name == "SERVER_INFO":
                     sys.stdout.write(Fore.YELLOW + Style.BRIGHT)
-                    sys.stdout.write("INFO> ")
+                    sys.stdout.write(f"{Fore.RED}{Style.BRIGHT}INFO> ")
                 elif name == "PEOPLE_ONLINE":
                     self.print_list("PEOPLE ONLINE", msg)
                 else:
@@ -105,11 +112,8 @@ class ChatClient:
                 print(problem + f"Error processing message: {e}")
 
     def handle_user_input(self, msg):
-        if msg == "EXIT":
-            self.disconnect()
-        elif msg == "CLEAR":
-            print("\x1b[2J\x1b[H")
-            self.clear_current_line()
+        if msg == "CLEAR":
+            system('clear')
         elif msg == "HELP":
             print(self.helpMsg)
         elif msg == "LIST":
@@ -119,7 +123,7 @@ class ChatClient:
                 self.s.send((self.group + self.separator + msg).encode())
             except socket.error as e:
                 print(problem + f"Send error: {e}")
-                self.disconnect()
+                self.disconnect("main")
 
     def setup_chat_room(self):
         try:
@@ -129,9 +133,10 @@ class ChatClient:
             if groupName:
                 self.group = groupName
             self.s.send(self.group.encode())
+        except KeyboardInterrupt:
+            self.disconnect("main")
         except TimeoutError:
-            print("Please, Wait for the other person to validate")  # Need fix queue
-            sys.exit(0)
+            self.disconnect("cork")
 
     def get_username(self):
         while not self.username:
@@ -143,13 +148,12 @@ class ChatClient:
         firstConnResponse = self.s.recv(self.RECV_BUFFER).decode()
         if f"SERVER_FAIL{self.separator}" in firstConnResponse:
             print(f"{Fore.RED}{Style.BRIGHT}ERROR> Cannot have same names{Fore.RESET}")
-            self.disconnect()
+            self.disconnect("main")
         else:
-            print(f"{Fore.YELLOW}{Style.BRIGHT}INFO>{Fore.YELLOW} "
-                  f"Connected to host. Start sending messages.{Fore.RESET}")
+            system('clear')
             print(self.helpMsg)
-            print(f"{Fore.MAGENTA}Joined {Fore.YELLOW}{self.group}{Fore.MAGENTA} "
-                  f"group as {Fore.YELLOW}{self.username}{Fore.RESET}")
+            print(f"{Fore.MAGENTA}Joined in group {Fore.YELLOW}{self.group}{Fore.MAGENTA}"
+                  f" as {Fore.YELLOW}<{self.username}>{Fore.RESET}\n")
 
     def run(self):
         self.connect()
@@ -158,22 +162,26 @@ class ChatClient:
         self.handle_first_connection()
 
         while True:
-            self.prompt()
-            socket_list = [sys.stdin, self.s]
-            read_sockets, _, _ = select.select(socket_list, [], [])
-            for sock in read_sockets:
-                if sock == self.s:
-                    try:
-                        data = self.s.recv(self.RECV_BUFFER).decode('utf-8')
-                        if not data:
-                            self.disconnect()
-                        self.handle_incoming_message(data)
-                    except socket.error as e:
-                        print(problem + f"Receive error: {e}")
-                        self.disconnect()
-                else:
-                    msg = sys.stdin.readline().strip()
-                    self.handle_user_input(msg)
+            try:
+                sys.stdout.write(f"{Fore.GREEN}{Style.BRIGHT}> {Fore.RESET}")
+                sys.stdout.flush()
+                socket_list = [sys.stdin, self.s]
+                read_sockets, _, _ = select.select(socket_list, [], [])
+                for sock in read_sockets:
+                    if sock == self.s:
+                        try:
+                            data = self.s.recv(self.RECV_BUFFER).decode('utf-8')
+                            if not data:
+                                self.disconnect("main")
+                            self.handle_incoming_message(data)
+                        except socket.error as e:
+                            print(problem + f"Receive error: {e}")
+                            self.disconnect("main")
+                    else:
+                        msg = sys.stdin.readline().strip()
+                        self.handle_user_input(msg)
+            except KeyboardInterrupt:
+                self.disconnect("main")
 
 
 if __name__ == "__main__":
